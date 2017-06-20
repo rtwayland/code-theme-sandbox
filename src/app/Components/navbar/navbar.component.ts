ab483369a03af5f0f87d6b7dcfb658e41d56abff
Raleigh Wayland <rtwayland@gmail.com>
2017-06-20 12:28:15 -0600

    nav component communicating with app and console

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @Output() onModuleSelected: EventEmitter<string> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
 
+  setCodeSelection(language: string) {
+    // console.log(language);
+    this.onModuleSelected.emit(language);
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { log } from "util";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  selectedModule: string = 'js'
+
+  onModuleSelected(language: string) {
+    this.selectedModule = language;
+  }
 }
