81bc109a6b2b88bc85dea7f94dd3bff1e79a4dfb
Raleigh Wayland <rtwayland@gmail.com>
2017-06-19 22:47:18 -0600

    Adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ConsoleComponent } from './Components/console/console.component';
+import { SelectionControlsComponent } from './Components/selection-controls/selection-controls.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsoleComponent,
+    SelectionControlsComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
