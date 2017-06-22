5fe895c6c1e3e05f9a3adaff26af62f63d779dc8
rtwayland <rtwayland@gmail.com>
2017-06-22 13:50:23 -0600

    Cleaning up code

--- src/app/Components/console/html-sandbox/html-sandbox.component.ts
+++ src/app/Components/console/html-sandbox/html-sandbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-html-sandbox',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./html-sandbox.component.scss']
 })
 export class HtmlSandboxComponent implements OnInit {
-
+  @Input() theme: {};
   constructor() { }
 
   ngOnInit() {
